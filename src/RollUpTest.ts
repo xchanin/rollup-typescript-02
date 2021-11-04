81b703ae17e58138d2a328328e25de6d7c85b14e
xchanin <shannon.bruns@fathym.com>
2021-11-03 23:05:22 -0600

    rerender canvas on module change

--- src/RollUpTest.ts
+++ src/RollUpTest.ts
@@ -1,9 +1,4 @@
-import { CanvasControl } from './controls/Canvas.control';
 import { EventsUtils } from './utils/events.utils';
-/**
- * TODO: figure out why setter is being called twice - MenuItems
- */
-
 import { html, LitElement } from 'lit';
 import { property, queryAsync, state } from 'lit/decorators.js';
 import { query } from 'lit/decorators/query.js';
@@ -15,22 +10,23 @@ import { PositionUtils } from './utils/position.utils.js';
 import { NodeTemplates } from './templates/node-templates.js';
 import { ModuleMenuModel } from './models/menu/module-menu.model.js';
 export class RollUpTest extends LitElement {
-  
-  @property({ type: Number }) 
-  public Counter: number;
 
+  /**
+   * Get the canvas element
+   *
+   * @query is a decorator to access nodes in the component’s shadow root
+   * equivalant to: this.renderRoot.querySelector('#first');
+   * can't use document.querySelector('#first') in shadow root
+   */
   @query('#drawflow')
   public Canvas!: HTMLElement;
 
-  @queryAsync('#drawflow')
-  public CanvasAsync!: CanvasControl;
-
   @query('#drag-items')
   public DragItems: HTMLElement;
-  
+
   private _flowData: DataFlowDataModel;
 
-  @property({ type: DataFlowDataModel }) 
+  @property({ type: DataFlowDataModel })
   public set FlowData(val: DataFlowDataModel) {
     this._flowData = val;
   }
@@ -38,6 +34,13 @@ export class RollUpTest extends LitElement {
   public get FlowData(): DataFlowDataModel {
     return this._flowData;
   }
+
+  /**
+   * Internal reactive state refers to properties that aren't
+   * part of the component's API. Typically marked protected or private
+   */
+  // @state()
+  protected flowTool: FlowTool;
 
   @property({ type: String })
   public HeaderTitle: string;
@@ -55,31 +58,23 @@ export class RollUpTest extends LitElement {
   @query("#tab-menu")
   public TabMenu: HTMLElement;
 
-  @property({ type: String }) 
-  public Title: string;
-
-  /**
-   * Internal reactive state refers to properties that aren't
-   * part of the component's API. Typically marked protected or private
-   */
-  // @state()
-  protected flowTool: any;
-
   constructor() {
     super();
 
     /**
-     * Trying to make thing rerender when the module changes
+     * Listen for module changed event
      */
-    EventsUtils.OnEvent('moduleChanged', () => {
-      alert('MODUEL CHANGED');
-     // this.requestUpdate();
+    EventsUtils.OnEvent('moduleChanged', (moduleName: string) => {
+
+     /**
+      * On module change, rerender the canvas with new module data
+      * */
+     this.requestUpdate();
     })
-   
 
     /**
      * TODO: understand why I have to do this
-     * 
+     *
      * At this point the.Canvas doesn't exist, have to wait for things
      * to fully render - need to wait from updateComplete for this.Canvas
      * to be there, but updateComplete doesn't fire unless a property on this
@@ -95,16 +90,16 @@ export class RollUpTest extends LitElement {
       this.flowTool = new FlowTool(this.Canvas);
       this.flowTool.Init(this.FlowData);
     })
-    
+
   }
 
   protected addNodeToDrawFlow(name: string, x: number, y: number): any {
-    
+
     if (VariablesUtils.EditorMode === 'fixed') {
       return false;
     }
 
-    let posX = PositionUtils.DraggedNodeEndXPos(x);	
+    let posX = PositionUtils.DraggedNodeEndXPos(x);
     let posY = PositionUtils.DraggedNodeEndYPos(y);
 
     switch (name) {
@@ -114,13 +109,13 @@ export class RollUpTest extends LitElement {
           {
             ID: '1',
             AllowedOutputTypes: ['PROJECT'],
-            Name: 'request', 
-            NumOfInputs: 0, 
-            NumOfOutputs: 1, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: [], 
-            Data: {}, 
+            Name: 'request',
+            NumOfInputs: 0,
+            NumOfOutputs: 1,
+            PosX: posX,
+            PosY: posY,
+            ClassList: [],
+            Data: {},
             HTML: NodeTemplates.RequestTemplate,
             // HTML: document.getElementById('request').content,
             TypeNode: false
@@ -134,9 +129,9 @@ export class RollUpTest extends LitElement {
             Type: 'project',
             AllowedInputTypes: ['REQUEST'],
             ID: '2',
-            Name: 'project', 
-            NumOfInputs: 1, 
-            NumOfOutputs: 1, 
+            Name: 'project',
+            NumOfInputs: 1,
+            NumOfOutputs: 1,
             Outputs: {
               "output_1":
               {
@@ -144,10 +139,10 @@ export class RollUpTest extends LitElement {
                       []
               }
             },
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: [], 
-            Data: {}, 
+            PosX: posX,
+            PosY: posY,
+            ClassList: [],
+            Data: {},
             HTML: NodeTemplates.ProjectTemplate,
             TypeNode: false
           }
@@ -158,13 +153,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '3',
-            Name: 'filter', 
-            NumOfInputs: 1, 
-            NumOfOutputs: 1, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: [], 
-            Data: {}, 
+            Name: 'filter',
+            NumOfInputs: 1,
+            NumOfOutputs: 1,
+            PosX: posX,
+            PosY: posY,
+            ClassList: [],
+            Data: {},
             HTML: NodeTemplates.RouteTemplate,
             TypeNode: false
           }
@@ -175,13 +170,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '3',
-            Name: 'application', 
-            NumOfInputs: 0, 
-            NumOfOutputs: 1, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: [], 
-            Data: {}, 
+            Name: 'application',
+            NumOfInputs: 0,
+            NumOfOutputs: 1,
+            PosX: posX,
+            PosY: posY,
+            ClassList: [],
+            Data: {},
             HTML: NodeTemplates.ApplicationTemplate,
             TypeNode: false
           }
@@ -192,13 +187,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '4',
-            Name: 'modifier', 
-            NumOfInputs: 0, 
-            NumOfOutputs: 1, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: [], 
-            Data: {}, 
+            Name: 'modifier',
+            NumOfInputs: 0,
+            NumOfOutputs: 1,
+            PosX: posX,
+            PosY: posY,
+            ClassList: [],
+            Data: {},
             HTML: NodeTemplates.ModifierTemplate,
             TypeNode: false
           }
@@ -209,13 +204,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '5',
-            Name: 'join', 
-            NumOfInputs: 1, 
-            NumOfOutputs: 2, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: [], 
-            Data: {}, 
+            Name: 'join',
+            NumOfInputs: 1,
+            NumOfOutputs: 2,
+            PosX: posX,
+            PosY: posY,
+            ClassList: [],
+            Data: {},
             HTML: NodeTemplates.JoinTemplate,
             TypeNode: false
           }
@@ -226,13 +221,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '6',
-            Name: 'split', 
-            NumOfInputs: 2, 
-            NumOfOutputs: 2, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: [], 
-            Data: {}, 
+            Name: 'split',
+            NumOfInputs: 2,
+            NumOfOutputs: 2,
+            PosX: posX,
+            PosY: posY,
+            ClassList: [],
+            Data: {},
             HTML: NodeTemplates.SplitTemplate,
             TypeNode: false
           }
@@ -243,13 +238,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '7',
-            Name: 'decision', 
-            NumOfInputs: 1, 
-            NumOfOutputs: 1, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: [], 
-            Data: {}, 
+            Name: 'decision',
+            NumOfInputs: 1,
+            NumOfOutputs: 1,
+            PosX: posX,
+            PosY: posY,
+            ClassList: [],
+            Data: {},
             HTML: NodeTemplates.DecisionTemplate,
             TypeNode: false
           }
@@ -260,13 +255,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '8',
-            Name: 'event', 
-            NumOfInputs: 0, 
-            NumOfOutputs: 1, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: [], 
-            Data: {}, 
+            Name: 'event',
+            NumOfInputs: 0,
+            NumOfOutputs: 1,
+            PosX: posX,
+            PosY: posY,
+            ClassList: [],
+            Data: {},
             HTML: NodeTemplates.EventTemplate,
             TypeNode: false
           }
@@ -277,13 +272,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '9',
-            Name: 'facebook', 
-            NumOfInputs: 0, 
-            NumOfOutputs: 1, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: ['facebook'], 
-            Data: {}, 
+            Name: 'facebook',
+            NumOfInputs: 0,
+            NumOfOutputs: 1,
+            PosX: posX,
+            PosY: posY,
+            ClassList: ['facebook'],
+            Data: {},
             HTML: NodeTemplates.FacebookTemplate,
             TypeNode: false
           }
@@ -294,16 +289,16 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '10',
-            Name: 'slack', 
-            NumOfInputs: 1, 
-            NumOfOutputs: 0, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: ['slack'], 
-            Data: {}, 
+            Name: 'slack',
+            NumOfInputs: 1,
+            NumOfOutputs: 0,
+            PosX: posX,
+            PosY: posY,
+            ClassList: ['slack'],
+            Data: {},
             HTML: NodeTemplates.SlackTemplate,
             TypeNode: false
-          }  
+          }
         );
         break;
       case 'github':
@@ -311,13 +306,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '11',
-            Name: 'github', 
-            NumOfInputs: 0, 
-            NumOfOutputs: 1, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: ['github'], 
-            Data: {}, 
+            Name: 'github',
+            NumOfInputs: 0,
+            NumOfOutputs: 1,
+            PosX: posX,
+            PosY: posY,
+            ClassList: ['github'],
+            Data: {},
             HTML: NodeTemplates.GithubTemplate,
             TypeNode: false
           }
@@ -328,13 +323,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '12',
-            Name: 'telegram', 
-            NumOfInputs: 0, 
-            NumOfOutputs: 1, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: ['telegram'], 
-            Data: {}, 
+            Name: 'telegram',
+            NumOfInputs: 0,
+            NumOfOutputs: 1,
+            PosX: posX,
+            PosY: posY,
+            ClassList: ['telegram'],
+            Data: {},
             HTML: NodeTemplates.TelegramTemplate,
             TypeNode: false
           }
@@ -345,13 +340,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '13',
-            Name: 'aws', 
-            NumOfInputs: 0, 
-            NumOfOutputs: 1, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: ['aws'], 
-            Data: {}, 
+            Name: 'aws',
+            NumOfInputs: 0,
+            NumOfOutputs: 1,
+            PosX: posX,
+            PosY: posY,
+            ClassList: ['aws'],
+            Data: {},
             HTML: NodeTemplates.AWSTemplate,
             TypeNode: false
           }
@@ -362,13 +357,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '14',
-            Name: 'log', 
-            NumOfInputs: 1, 
-            NumOfOutputs: 1, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: ['log'], 
-            Data: {}, 
+            Name: 'log',
+            NumOfInputs: 1,
+            NumOfOutputs: 1,
+            PosX: posX,
+            PosY: posY,
+            ClassList: ['log'],
+            Data: {},
             HTML: NodeTemplates.LogTemplate,
             TypeNode: false
           }
@@ -379,13 +374,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '15',
-            Name: 'google', 
-            NumOfInputs: 1, 
-            NumOfOutputs: 0, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: ['google'], 
-            Data: {}, 
+            Name: 'google',
+            NumOfInputs: 1,
+            NumOfOutputs: 0,
+            PosX: posX,
+            PosY: posY,
+            ClassList: ['google'],
+            Data: {},
             HTML: NodeTemplates.GoogleTemplate,
             TypeNode: false
           }
@@ -396,13 +391,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '16',
-            Name: 'email', 
-            NumOfInputs: 1, 
-            NumOfOutputs: 0, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: ['email'], 
-            Data: {}, 
+            Name: 'email',
+            NumOfInputs: 1,
+            NumOfOutputs: 0,
+            PosX: posX,
+            PosY: posY,
+            ClassList: ['email'],
+            Data: {},
             HTML: NodeTemplates.EmailTemplate,
             TypeNode: false
           }
@@ -413,13 +408,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '17',
-            Name: 'template', 
-            NumOfInputs: 1, 
-            NumOfOutputs: 1, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: ['template'], 
-            Data: {}, 
+            Name: 'template',
+            NumOfInputs: 1,
+            NumOfOutputs: 1,
+            PosX: posX,
+            PosY: posY,
+            ClassList: ['template'],
+            Data: {},
             HTML: NodeTemplates.TemplateTemplate,
             TypeNode: false
           }
@@ -430,13 +425,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '18',
-            Name: 'multiple', 
-            NumOfInputs: 3, 
-            NumOfOutputs: 4, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: ['multiple'], 
-            Data: {}, 
+            Name: 'multiple',
+            NumOfInputs: 3,
+            NumOfOutputs: 4,
+            PosX: posX,
+            PosY: posY,
+            ClassList: ['multiple'],
+            Data: {},
             HTML: NodeTemplates.MultipleTemplate,
             TypeNode: false
           }
@@ -447,13 +442,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '19',
-            Name: 'personalized', 
-            NumOfInputs: 1, 
-            NumOfOutputs: 1, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: ['personalized'], 
-            Data: {}, 
+            Name: 'personalized',
+            NumOfInputs: 1,
+            NumOfOutputs: 1,
+            PosX: posX,
+            PosY: posY,
+            ClassList: ['personalized'],
+            Data: {},
             HTML: NodeTemplates.PersonalizedTemplate,
             TypeNode: false
           }
@@ -464,13 +459,13 @@ export class RollUpTest extends LitElement {
         (
           {
             ID: '20',
-            Name: 'dbclick', 
-            NumOfInputs: 0, 
-            NumOfOutputs: 1, 
-            PosX: posX, 
-            PosY: posY, 
-            ClassList: ['dbclick'], 
-            Data: {}, 
+            Name: 'dbclick',
+            NumOfInputs: 0,
+            NumOfOutputs: 1,
+            PosX: posX,
+            PosY: posY,
+            ClassList: ['dbclick'],
+            Data: {},
             HTML: NodeTemplates.DBLClickTemplate,
             TypeNode: false
           }
@@ -482,7 +477,7 @@ export class RollUpTest extends LitElement {
   }
 
   protected dragEvent(eventType: string, e: any): void {
-    
+
     console.log('DragEvent');
 
     switch(eventType) {
@@ -495,15 +490,15 @@ export class RollUpTest extends LitElement {
         e.preventDefault();
         break;
     }
-    
+
   }
 
   /**
    * Called after the element’s DOM has been updated the first time, immediately before updated is called.
-   * 
+   *
    * @param changedProperties Map - keys are the names of change properties
    * Values are the corresponding previous values
-   * 
+   *
    * Property changes inside this method will trigger an element update
    */
   protected firstUpdated(changedProperties: any): void {
@@ -523,7 +518,9 @@ export class RollUpTest extends LitElement {
    * with the `html` helper function.
    */
   render() {
-    return html `
+    return this.FlowData
+    ? html `<bold> No Flow Data </bold>`
+    : html `
 
     <link rel="stylesheet" href="./assets/styles/global-scss.min.css">
 
@@ -532,12 +529,12 @@ export class RollUpTest extends LitElement {
     </header>
 
     <div class="wrapper">
-    
-      <!-- 
+
+      <!--
         Left side menu items
       -->
-      <drag-menu-items 
-        id="drag-items" 
+      <drag-menu-items
+        id="drag-items"
         class="col"
         .MenuItems="${ this.SideMenuItems }">
       </drag-menu-items>
@@ -545,18 +542,18 @@ export class RollUpTest extends LitElement {
       <div class="col-right">
 
         <!--Tab Menu-->
-        <tab-menu-control 
+        <tab-menu-control
           id="tab-menu"
           .TabItems=${ this.TabMenuItems }
           .ChangeModuleEvent=${ this.flowTool.ChangeModule.bind(this.flowTool) }>
         </tab-menu-control>
 
-        <!-- 
+        <!--
           Canvas Area
          -->
         <canvas-control
           id="drawflow"
-          @drop="${ (e: DragEvent) => this.dragEvent('drop', e) }" 
+          @drop="${ (e: DragEvent) => this.dragEvent('drop', e) }"
           @dragover="${ (e: DragEvent) => this.dragEvent('dragover', e) }">
         </canvas-control>
       </div>
